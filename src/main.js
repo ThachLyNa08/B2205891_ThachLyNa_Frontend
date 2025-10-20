ebafcb0452db943081040fb5d5fad68256d7e240
Thach Ly Na B2205891 <151183925+ThachLyNa08@users.noreply.github.com>
2025-10-20 14:34:38 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import "./assets/form.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@fortawesome/fontawesome-free/js/all.min.js";
